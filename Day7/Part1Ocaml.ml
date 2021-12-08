74927fc062f9eb81cec3a3fb869f38f7b14200f8
cgccuser <0foobarbaz@gmail.com>
2021-12-07 20:41:45 -0500

    Do day 7 part 1

---
+++ Day7/Part1Ocaml.ml
@@ -0,0 +1,23 @@
+let read_lines name : string list =
+  let ic = open_in name in
+  let try_read () =
+    try Some (input_line ic) with End_of_file -> None in
+  let rec loop acc = match try_read () with
+    | Some s -> loop (s :: acc)
+    | None -> close_in ic; List.rev acc in
+  loop []
+
+let sum = List.fold_left (+) 0
+let min = List.fold_left (fun a b -> if a < b then a else b) Int.max_int
+let max = List.fold_left (fun a b -> if a > b then a else b) Int.min_int
+let rec range a b = if a <= b then a :: range (a+1) b else []
+
+let main _ =
+  let (input :: _) = read_lines "input.txt" in
+  let nums = List.map int_of_string (String.split_on_char ',' input) in
+  let cost x = sum (List.map (fun d -> Int.abs (d - x)) nums) in
+  let min_cost = min (List.map cost (range (min nums) (max nums))) in
+  print_string (string_of_int min_cost) ;
+  print_string "\n";;
+
+main ();;
